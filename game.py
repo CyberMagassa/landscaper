878c0a8313ae646e4b844d2359a940b58da8ba75
username <youremail@domain.com>
2023-06-29 15:14:37 -0400

    Scissors Item Added

--- game.py
+++ game.py
@@ -1,13 +1,28 @@
 total_earnings = 0
+earnings_rate = 1
 
 while True:
-    user_input = input('Do you want to cut a lawn? (yes/no): ')
-    
-    if user_input.lower() =='yes':
-        total_earnings +=1
-        print('You cut a lawn! Your total eanrnings: $', total_earnings)
-    elif user_input.lower() == 'no':
-        print('You decided to stop working. Your total earnings for the day: $', total_earnings)
+    user_input = input("What would you like to do? Enter 'work' to cut a lawn, 'buy' to purchase scissors, or 'quit' to stop for the day: ")
+
+    if user_input.lower() == "work":
+        total_earnings += earnings_rate
+        print("You cut a lawn! Your total earnings: $", total_earnings)
+        print()  # Line break
+    elif user_input.lower() == "buy":
+        cost = 10  # Cost of purchasing scissors
+        if total_earnings >= cost:
+            total_earnings -= cost
+            earnings_rate += 4
+            print("You purchased scissors! Your earnings rate has increased.")
+            print("Your current earnings rate: ", earnings_rate)
+        else:
+            print("Insufficient funds to purchase scissors.")
+        print()  # Line break
+    elif user_input.lower() == "quit":
+        print("You decided to stop working. Your total earnings for the day: $", total_earnings)
         break
     else:
-        print("Invalid input. Please enter a 'yes'or a 'no'")
+        print("Invalid input. Please enter 'work', 'buy', or 'quit'.")
+        print()  # Line break
+
+
